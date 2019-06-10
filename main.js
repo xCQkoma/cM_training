45c5fbca4796ab97b50de89e993829fa9bcf874a
xCQkoma <aa4sui2ren5@gmail.com>
2019-06-11 00:34:56 +0900

    add bounce

--- main.js
+++ main.js
@@ -17,5 +17,26 @@ window.onload = function(){
     context.beginPath();
     context.arc(p.position.getX(), p.position.getY(), p.radius, 0, Math.PI*2, false);
     context.fill();
+
+    if(p.position.getX() + p.radius > width){
+      p.position.setX(width - p.radius);
+      p.velocity.setX(p.velocity.getX()*p.bounce);
+    }
+    if(p.position.getX() - p.radius < 0){
+      p.position.setX(p.radius);
+      p.velocity.setX(p.velocity.getX()*p.bounce);
+    }
+
+    if(p.position.getY() + p.radius > height){
+      p.position.setY(height - p.radius);
+      p.velocity.setY(p.velocity.getY()*p.bounce);
+    }
+    if(p.position.getY() - p.radius < 0){
+      p.position.setY(p.radius);
+      p.velocity.setY(p.velocity.getY()*p.bounce);
+    }
+
+    requestAnimationFrame(update);
+
   }
 }
